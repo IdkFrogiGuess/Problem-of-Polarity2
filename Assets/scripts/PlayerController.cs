79c8c757223836b7faac9d11524c5689bd1c4ee2
MUKILTEO\1726098 <uberfrogguy@gmail.com>
2025-11-20 09:13:04 -0800

    fixed player controls

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -12,26 +12,73 @@ public class PlayerController : MonoBehaviour
     public float checkRadius;
     public LayerMask whatIsGround;
 
-    private
+    private float jumpTimeCounter;
+    public float jumpTime;
+    private bool isJumping;
+
+    public bool InvertMovement;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
+
     void FixedUpdate()
     {
+        // Read input and invert if requested
         MoveInput = Input.GetAxis("Horizontal");
+        if (InvertMovement)
+        {
+            MoveInput = -MoveInput;
+        }
+
+        // Apply horizontal movement each physics update
         rb.linearVelocity = new Vector2(MoveInput * speed, rb.linearVelocity.y);
     }
+
     void Update()
     {
         isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
-        if (isGrounded == true && Input.GetKeyDown(KeyCode.Space))
-        {
-            rb.linearVelocity = Vector2.up * jumpForce;
-        }
-        if (Input.GetKey(KeyCode.Space)){
-            rb.linearVelocity = Vector2.up * jumpForce;
 
+        if (MoveInput > 0)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
         }
+        else if (MoveInput < 0)
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+
+        if (isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            isJumping = true;
+            jumpTimeCounter = jumpTime;
+            rb.linearVelocity = Vector2.up * jumpForce;
+        }
+
+        if (Input.GetKey(KeyCode.Space) && isJumping)
+        {
+            if (jumpTimeCounter > 0)
+            {
+                rb.linearVelocity = Vector2.up * jumpForce;
+                jumpTimeCounter -= Time.deltaTime;
+            }
+            else
+            {
+                isJumping = false;
+            }
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            isJumping = false;
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if (feetPos == null) return;
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(feetPos.position, checkRadius);
     }
 }
