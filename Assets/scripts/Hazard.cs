9f55395767ac813acc8c8662045429560bd59c5a
MUKILTEO\1725934 <leavittjonathan8@gmail.com>
2025-11-25 10:15:28 -0800

    lvl1

--- Assets/scripts/Hazard.cs
+++ Assets/scripts/Hazard.cs
@@ -1,29 +1,49 @@
 using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Hazard : MonoBehaviour
 {
     public bool isDead = false;
+    public float reloadDelay = 3f; // seconds to wait before restarting the room
+
+    // Prevent multiple hazards from scheduling multiple reloads
+    private static bool s_reloadScheduled = false;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
-  void OnCollisionEnter2D(Collision2D collision)
+
+    // Called when this Collider2D/ Rigidbody2D has begun touching another Rigidbody2D/Collider2D
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        if (gameObject.CompareTag("Hazard"))
+        if (collision.gameObject.CompareTag("Hazard"))
         {
             isDead = true;
+            Destroy(gameObject);
         }
-        else
+    }
+
+    // Called when another Collider2D enters a trigger attached to this object (if either is marked as trigger)
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Hazard"))
         {
-            isDead = false;
+            isDead = true;
+            Destroy(gameObject);
         }
+    }
+
+    private void OnDestroy()
+    {
+        Debug.Log("Hazard destroyed");
+        
     }
 }
