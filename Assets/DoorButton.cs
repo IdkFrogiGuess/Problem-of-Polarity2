546934b933fbd03875e7d6a2fb5d0eae7b6ff70c
MUKILTEO\1726098 <uberfrogguy@gmail.com>
2025-11-26 09:14:58 -0800

    door physics and both types of buttons work wooo!!!!!!!!!!!

--- Assets/DoorButton.cs
+++ Assets/DoorButton.cs
@@ -3,20 +3,67 @@ using UnityEngine;
 public class DoorButton : MonoBehaviour
 {
     public bool buttonPressed = false;
-    public GameObject door;
+    public bool hasBeenOpened = false;
     
+    public GameObject door;
+    public bool isHeavy = false;
+    public Vector2 openPosition;
+    private Vector2 closedPosition;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(openPosition,door.transform.localScale);
+    }
+
+
+    private void Start()
+    {
+        closedPosition = door.transform.position;
+    }
     void Update()
     {
         if (buttonPressed)
         {
             door.transform.Translate(Vector3.up * Time.deltaTime * 2);
+            hasBeenOpened = true;
         }
-    }
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.CompareTag("Player"))
+        
+        if( hasBeenOpened && isHeavy && buttonPressed == false)
         {
-            buttonPressed = true;
+            door.transform.position = Vector2.Lerp( door.transform.position, closedPosition, Time.deltaTime*4);
+        }
+        if (hasBeenOpened && isHeavy && buttonPressed == true)
+        {
+            door.transform.position = Vector2.Lerp(door.transform.position, openPosition, Time.deltaTime * 2);
         }
     }
-}
+
+        void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.CompareTag("Player") && isHeavy == false)
+            {
+                buttonPressed = true;
+            }
+        }
+    
+        void OnTriggerStay2D(Collider2D other)
+        {
+            if (isHeavy && other.CompareTag("Heavy"))
+            {
+                buttonPressed = true;
+            }
+
+        }
+        void OnTriggerExit2D(Collider2D other)
+        {
+            if (isHeavy)
+            {
+                buttonPressed = false;
+            }
+
+        }
+    }
+
+
+
