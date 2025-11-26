fd6d50abb2fe8532e7b9e4efbec5615a92dfa502
1725363 <1725363@mukilteo.wednet.edu>
2025-11-26 09:57:33 -0800

    Exit door script still in work in progress

--- Assets/scripts/Exit Door.cs
+++ Assets/scripts/Exit Door.cs
@@ -1,16 +1,22 @@
+using UnityEditor.Rendering;
 using UnityEngine;
 
 public class ExitDoor : MonoBehaviour
 {
+    public GameObject Door1;
+    public GameObject Door2;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Start() 
     {
-        
+        Door1 = GameObject.GetComponent<FirstCheck>().checkFirst;
+        Door2 = GameObject.GetComponent<SecondCheck>().checkSecond;
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+       if (Door1 && Door2)
+        {
+
+        }
     }
+    
 }
