a41d8f1bd4e8df1276f325d4f6bae3c18dc46d6e
1725363 <1725363@mukilteo.wednet.edu>
2025-12-01 09:10:14 -0800

    DOORS

--- Assets/scripts/Exit Door.cs
+++ Assets/scripts/Exit Door.cs
@@ -1,22 +1,39 @@
+
+using Unity.Mathematics;
 using UnityEditor.Rendering;
 using UnityEngine;
 
 public class ExitDoor : MonoBehaviour
 {
-    public GameObject Door1;
-    public GameObject Door2;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start() 
-    {
-        Door1 = GameObject.GetComponent<FirstCheck>().checkFirst;
-        Door2 = GameObject.GetComponent<SecondCheck>().checkSecond;
-    }
+    
+
+    public FirstCheck Door1;
+    public SecondCheck Door2;
+    public bool ExitIsOpen = false;
+    public int NextLevel;
+    
+
     void Update()
     {
-       if (Door1 && Door2)
+        if (Door1.CheckFirst == true && Door2.CheckSecond == true )
         {
-
+            ExitIsOpen = true;
+        }
+        else
+        {
+            ExitIsOpen = false;
+        }
+        if (ExitIsOpen)
+        {
+            ExitOpen();
         }
     }
+
+    void ExitOpen()
+    {
+        Debug.Log("Exit Opened!");
     
+            UnityEngine.SceneManagement.SceneManager.LoadScene(NextLevel);
+        
+    }
 }
